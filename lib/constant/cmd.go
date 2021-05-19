ffba5389b22b4078f6ba8530c1bc913498ec672a
UsrAlpha <twx19975261083@gmail.com>
2021-05-19 23:21:08 +0800

    fix: default review N

--- lib/constant/cmd.go
+++ lib/constant/cmd.go
@@ -7,7 +7,7 @@ const (
 	CMD_FIND_DSP    = "[word/phrase] 寻找记录的单词或者词组"
 	CMD_HELP_DSP    = "打印帮助"
 	CMD_LIST_DSP    = "打印记录在数据库中的单词和词组"
-	CMD_REVIEW_DSP  = "[N] 复习单词,后接复习的数量(默认为20)"
-	CMD_REVIEWP_DSP = "[N] 复习词组,后接复习的数量(默认为20)"
+	CMD_REVIEW_DSP  = "[N] 复习单词,后接复习的数量(默认为30)"
+	CMD_REVIEWP_DSP = "[N] 复习词组,后接复习的数量(默认为30)"
 	CMD_OK_DSP      = "[word/phrase] 将单词或者词组设置为已经掌握，以后不再复习,已经掌握的会重新投入复习"
 )
