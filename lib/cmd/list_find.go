db3ceac06c547d1b70b8cdc96c9b4011277187f3
UsrAlpha <twx19975261083@gmail.com>
2021-05-21 18:04:01 +0800

    word color

--- lib/cmd/list_find.go
+++ lib/cmd/list_find.go
@@ -16,7 +16,7 @@ import (
 func FindWord(word string) {
 	found := false
 	fmt.Println()
-	color.Green(word)
+	color.Red(word)
 	fmt.Println()
 	for _, v := range serve.DB.Cwords.Cword {
 		if v.Cid.String == word {
