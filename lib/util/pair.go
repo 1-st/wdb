98dc68d2e8277615f65178ff618179d034870562
UsrAlpha <twx19975261083@gmail.com>
2021-09-09 00:58:50 +0800

    fix quick sort of alphabet

--- lib/util/pair.go
+++ lib/util/pair.go
@@ -35,34 +35,66 @@ func (l *PairList) RSort() {
 }
 
 func AlphaSort(arr []Pair, low int, high int) {
-	if high <= low {
+	if high < low {
 		return
 	}
-	j := partition(arr, low, high)
+	j := partition(&arr, low, high)
 	AlphaSort(arr, low, j-1)
 	AlphaSort(arr, j+1, high)
 }
 
-var partition = func(arr []Pair, low int, high int) int {
-	i, j := low+1, high
-	for true {
-		for arr[i].Name < arr[low].Name {
+////划分数组
+//func partition(a []Pair, begin int, last int) int {
+//	var x = a[last].Score
+//	var i = begin - 1
+//	for j := begin; j < last; j++ {
+//		if x <= a[j].Score {
+//			i = i + 1
+//			a[i], a[j] = a[j], a[i]
+//		}
+//	}
+//	a[i+1], a[last] = a[last], a[i+1]
+//
+//	return i + 1
+//}
+
+func partition(arr *[]Pair,left int,right int)int{
+	privot:=(*arr)[right].Name
+	i:=left-1
+	for j:=left;j<right;j++{
+		if (*arr)[j].Name<privot{
 			i++
-			if i == high {
-				break
-			}
+			temp:=(*arr)[i]
+			(*arr)[i]=(*arr)[j]
+			(*arr)[j]=temp
 		}
-		for arr[low].Name < arr[j].Name {
-			j--
-			if j == low {
-				break
-			}
-		}
-		if i >= j {
-			break
-		}
-		arr[i], arr[j] = arr[j], arr[i]
 	}
-	arr[low], arr[j] = arr[j], arr[low]
-	return j
+	temp:=(*arr)[i+1]
+	(*arr)[i+1]=(*arr)[right]
+	(*arr)[right]=temp
+	return i+1
 }
+
+//var partition = func(arr []Pair, low int, high int) int {
+//	i, j := low+1, high
+//	for true {
+//		for arr[i].Name < arr[low].Name {
+//			i++
+//			if i == high {
+//				break
+//			}
+//		}
+//		for arr[low].Name < arr[j].Name {
+//			j--
+//			if j == low {
+//				break
+//			}
+//		}
+//		if i >= j {
+//			break
+//		}
+//		arr[i], arr[j] = arr[j], arr[i]
+//	}
+//	arr[low], arr[j] = arr[j], arr[low]
+//	return j
+//}
